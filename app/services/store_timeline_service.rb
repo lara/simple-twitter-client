51cee491cd73e5d33698103b706a80baa00b6d32
Lara Aydin <ayd.dilara@gmail.com>
2018-02-27 15:43:41 -0500

    Refactor

--- app/services/store_timeline_service.rb
+++ app/services/store_timeline_service.rb
@@ -1,27 +1,35 @@
 class StoreTimelineService
   attr_reader :client, :user
+  attr_accessor :page
 
   def initialize(client, user)
     @client = client
     @user = user
+    @page = 1
   end
 
   def store
-    page = 1
       loop do
         begin
-          tweets = client.user_timeline(user.uid.to_i, count: 2, page: page, tweet_mode: "extended")
           begin
-            tweets.each(&method(:save_tweet))
+            save_tweets
           rescue ActiveRecord::RecordNotUnique
             break
           end
-          page += 1
+          self.page += 1
           break if tweets.empty?
         rescue Twitter::Error::TooManyRequests => error
           sleep error.rate_limit.reset_in + 1
         end
       end
+  end
+
+  def save_tweets
+    tweets.each(&method(:save_tweet))
+  end
+
+  def tweets
+    client.user_timeline(user.uid.to_i, count: 2, page: page, tweet_mode: "extended")
   end
 
   def save_tweet(tweet)
